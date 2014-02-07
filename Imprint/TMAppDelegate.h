3d41e8aaeebc3d39a962e436e886aa4063ef9d1e
trmchale1 <trmchale@gmail.com>
2014-02-07 09:52:32 -0500

    Initial Commit

---
+++ Imprint/TMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMAppDelegate.h
+//  Imprint
+//
+//  Created by Tim McHale on 2/7/14.
+//  Copyright (c) 2014 Tim McHale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
